417ba7ec2de7b124713ff9d0acd1f833ac10432d
ipolakovic <ivanpolakovic2@gmail.com>
2020-12-07 15:35:15 +0100

    Update Data_manipulation.R

--- Data manipulation/Data_manipulation.R
+++ Data manipulation/Data_manipulation.R
@@ -3,15 +3,48 @@
 #### this is the data manipulation file used for the predictive analysis on R__solar system ###
 
 # install outliers package
-install.packages("outliers")
 library(outliers)
 library(dplyr)
+library(tidyr)
+library(purrr)
 
 #test on a subset, with only numeric args
-test_dt <- outlier(solar_data_num, opposite =  TRUE)
-rm.outlier(test_dt, fill = FALSE)  #should we remove outliers?
-rm.outlier(test_dt, fill = TRUE, median = TRUE) #should we replace with the median/mean? 
+test_dt <- solar_data_dt
+
+#test_dt <- outlier(solar_data_num, opposite =  TRUE)
+#rm.outlier(test_dt, fill = FALSE)  #should we remove outliers?
+#rm.outlier(test_dt, fill = TRUE, median = TRUE) #should we replace with the median/mean? 
+
 #should we remove more than 1 outlier? basically we would need to remove the 1st and run the function again
+
+# 2nd try
+
+remove_outliers <- function(x, na.rm = TRUE, ...) {
+  qnt <- quantile(x, probs=c(.25, .75), na.rm = na.rm, ...)
+  H <- 3 * IQR(x, na.rm = na.rm)
+  y <- x
+  y[x < (qnt[1] - H)] <- NA
+  y[x > (qnt[2] + H)] <- NA
+  y
+}
+
+test_dt2 <- test_dt[,2:99]
+test_dt2 <- as.data.table(apply(test_dt2, MARGIN = 2, FUN = remove_outliers))
+
+identical(test_dt,test_dt2)
+
+
+# 3rd try, this works, just not sure how to check how many outliers have been replaced
+# we could aso use  3* IQR(x, na.rm = na.rm) to focus only on extreme cases
+
+outlierreplacement <- function(dataframe){
+  dataframe %>%          
+    map_if(is.numeric, ~ replace(.x, .x %in% boxplot.stats(.x)$out, NA)) %>%
+    bind_cols 
+}
+outlierreplacement(test_dt)
+
+
 
 #############
 # DATES #
